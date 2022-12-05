31b2045e7a8b218a28395c36c0c023ceb3c9c4d4
syfirawdy <syafirawidiyanti11@gmail.com>
2022-12-05 19:05:56 +0700

    add modal on product detail

--- src/ProductDetail.js
+++ src/ProductDetail.js
@@ -9,11 +9,29 @@ const data = {
   productPrice: "1.000.000",
   productDesc:
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mi eros, mattis eu magna sed, posuere laoreet dui. Vestibulum auctor luctus arcu id tristique. Donec interdum placerat eleifend. Sed et nibh nisl. Donec lacinia tortor porta dui rutrum mattis. Nullam fermentum urna dignissim dapibus cursus. Sed lacinia justo in mi mattis, id laoreet est gravida.",
+  productSpec: [
+    {
+      Merk: "Beta Three", 
+      Watt: "24 Watt", 
+      Voltase: "240 V", 
+      Bluetooth: "No", 
+      AudioCrossover: "2 Way", 
+      Sensitivitas: "128 dB", 
+      KoneksiInput: "Jack Akai", 
+      MinimumFrekuensi: "52 Hz"
+    }
+  ],
+  productNote: 
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mi eros, mattis eu magna sed, posuere laoreet dui. Vestibulum auctor luctus arcu id tristique. Donec interdum placerat eleifend. Sed et nibh nisl. Donec lacinia tortor porta dui rutrum mattis. Nullam fermentum urna dignissim dapibus cursus. Sed lacinia justo in mi mattis, id laoreet est gravida.",
+
 };
 
 const initialState = 1;
+
 function ProductDetail() {
   const [qty, setQty] = useState(initialState);
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <div>
       <div className="my-8 mx-12">
@@ -80,12 +98,75 @@ function ProductDetail() {
                 <h6 className="text-xl font-semibold mb-4">Tentang Barang</h6>
                 <p className="text-xl leading-7 mb-6">
                   {data.productDesc}
-                  <Link to="/rincian" className="read-more">
+                  <Link onClick={() => setShowModal(true)} className="read-more">
                     {" "}
                     <br/>
                     Read More....
                   </Link>
                 </p>
+
+                {showModal ? (
+                  <>
+                    <div className="justify-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+                      <div className="relative w-1/2 my-6 mx-auto">
+                        {/*content*/}
+                        <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                          {/*header*/}
+                          <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                            <h3 className="text-3xl font-semibold">
+                              {data.productName}
+                            </h3>
+                            <button
+                              className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                              onClick={() => setShowModal(false)}
+                            >
+                              <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                                ×
+                              </span>
+                            </button>
+                          </div>
+                          {/*body*/}
+                          <div className="relative p-6 flex-auto">
+                            <p className="mt-4 mb-2 text-lg font-semibold leading-relaxed">
+                              Deskripsi Produk
+                            </p>
+                            <p className="text-slate-500 leading-relaxed">
+                              {data.productDesc}
+                            </p>
+                          </div>
+                          <div className="relative p-6 flex-auto">
+                            <p className="mb-2 text-lg font-semibold leading-relaxed">
+                              Spesifikasi Produk
+                            </p>
+                            <p className="mb-2 text-slate-500 leading-relaxed">
+                              {data.productDesc}
+                            </p>
+                          </div>
+                          <div className="relative p-6 flex-auto">
+                            <p className="mb-2 text-lg font-semibold leading-relaxed">
+                              Penting untuk Diketahui
+                            </p>
+                            <p className="mb-2 text-slate-500 leading-relaxed">
+                              {data.productNote}
+                            </p>
+                          </div>
+                          {/*footer*/}
+                          <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                            <button
+                              className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                              type="button"
+                              onClick={() => setShowModal(false)}
+                            >
+                              Close
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                  </>
+                ) : null}
+
                 <div className="flex flex-row gap-4 items-center">
                   <h6 className="text-lg font-medium my-4">Kuantitas :</h6>
                   <div className="border rounded-full w-32 text-center">
